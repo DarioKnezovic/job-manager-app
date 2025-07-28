85b3851789440ed73b55658b69dc561fda08082c
DarioKnezovic <dknezovic997@gmail.com>
2025-07-28 18:07:59 +0200

    Implemented workers page

--- components/Dashboard/Worker.tsx
+++ components/Dashboard/Worker.tsx
@@ -1,22 +1,41 @@
 import { Card, List } from "react-native-paper";
 import React from "react";
 import { StyleSheet } from "react-native";
+import { router } from "expo-router";
 
 const workerTasks = [
-    { id: '1', title: 'Deliver Order #123', time: '10:00 AM' },
-    { id: '2', title: 'Pick up Order #124', time: '12:00 PM' },
+    { id: '1', title: 'Deliver Order #123', description: 'Broken dishwasher', status: 'complete' },
+    { id: '2', title: 'Pick up Order #124', description: 'Dunno', status: 'ready' },
 ];
 
 export default function Worker() {
+    const handleJobPress = (jobId: string) => {
+        router.push(`/jobs/${jobId}`);
+    };
+
+    const getBackgroundColor = (status: string) => {
+        switch (status) {
+            case 'complete':
+                return { backgroundColor: 'lightgreen' };
+            case 'doing':
+                return { backgroundColor: 'lightyellow' };
+            case 'ready':
+                return { backgroundColor: 'lightblue' };
+            default:
+                return {};
+        }
+    };
+
     return (
         <>
             <List.Section title="Today's Tasks">
                 {workerTasks.map((task) => (
-                    <Card key={task.id} style={styles.card}>
+                    <Card key={task.id} style={[styles.card, getBackgroundColor(task.status)]}>
                         <Card.Content>
                             <List.Item
+                                onPress={() => handleJobPress(task.id)}
                                 title={task.title}
-                                description={task.time}
+                                description={task.description}
                                 left={props => <List.Icon {...props} icon="clipboard-check-outline" />}
                             />
                         </Card.Content>
@@ -24,7 +43,7 @@ export default function Worker() {
                 ))}
             </List.Section>
         </>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
